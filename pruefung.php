2aada01c80743e215e5aa4c0ffdc4499052e3668
marco <git@mdriemel.de>
2012-09-12 19:04:46 +0200

    prüfungsfragen

---
+++ pruefung.php
@@ -0,0 +1,56 @@
+<?php
+/*
+ function hello()
+ {
+return 'test';
+}
+
+echo $txt = hello();
+*/
+/*
+ class testClass{
+function test(){
+return;
+}
+}
+
+testClass::test();
+*/
+// 4.17
+class halo{
+	public static $vari = "asdf<br/>";
+}
+echo halo::$vari;
+
+$object = new halo();
+echo $object::$vari;
+
+$klasse = 'halo';
+
+echo $klasse::$vari;
+
+/*
+Frage 12 Buch 11.8:
+Interface :  2
+Abstrakte : 1 Marco
+kann nicht realisiert werden: 1 Sven
+
+8.20
+Cookie setzen und http-request
+stream_setCookie
+file_get_contents
+fopen fpassthru
+
+12.17
+wurde mit PHP 5 eingefürt (2 Möglichkeiten)
+session.use_only_cookies			
+session.hash_function				
+session.cookie_secure				
+session.cache_expires				
+session.hash_bits_per_character 	
+
+9.5
+session löschen
+
+*/
+?>
